d24bfad0265f12ad381e0b77090d0377e3816b38
Luiz Eduardo <luizedupp@gmail.com>
2024-03-09 18:27:30 -0300

    Primeiro commit para api spring boot jpa

---
+++ src/test/java/com/monitoring/api/MonitoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
